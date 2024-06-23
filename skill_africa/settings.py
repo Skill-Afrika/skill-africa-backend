3440650fb28dd312d52ed3c0c6ee1e29809a374a
rabiu <medrabiu@gmail.com>
2024-06-23 19:05:05 +0100

    second_deploy

--- skill_africa/settings.py
+++ skill_africa/settings.py
@@ -21,7 +21,7 @@ SECRET_KEY = os.getenv("SECRET_KEY", "fallback-secret-key")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['your-render-app-url.onrender.com', 'localhost', '127.0.0.1']
+ALLOWED_HOSTS = ['backend-api-fq3o.onrender.com', 'localhost', '127.0.0.1']
 
 
 # Application definition
